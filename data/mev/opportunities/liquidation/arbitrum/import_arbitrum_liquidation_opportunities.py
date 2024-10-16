7b7c649ffc2c73f85eb9292d123ac2dd971fed96
Christof Ferreira Torres <christof@Christofs-MacBook-Pro.local>
2024-10-16 09:00:47 -0600

    Initial commit

---
+++ data/mev/opportunities/liquidation/arbitrum/import_arbitrum_liquidation_opportunities.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import os
+import pymongo
+
+MONGO_HOST = "localhost"
+MONGO_PORT = 27017
+
+def main():
+    mongo_connection = pymongo.MongoClient("mongodb://"+MONGO_HOST+":"+str(MONGO_PORT), maxPoolSize=None)
+    print("Importing results...")
+    for r, d, f in os.walk("."):
+        for file in f:
+            if file.endswith(".json"):
+                print(os.path.join(r, file))
+                os.system('mongoimport --uri="mongodb://'+MONGO_HOST+':'+str(MONGO_PORT)+'/arbitrum" --collection mev_liquidation_opportunities --type json --file '+file)
+    print("Done.")
+    total = mongo_connection["arbitrum"]["mev_liquidation_opportunities"].count_documents({})
+    print("Imported", total, "results.")
+
+
+if __name__ == "__main__":
+    main()
---
+++ data/mev/opportunities/liquidation/optimism/export_optimism_liquidation_opportunities.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import os
+import pymongo
+
+MONGO_HOST = "localhost"
+MONGO_PORT = 27017
+
+LIMIT = 10000
+
+def main():
+    mongo_connection = pymongo.MongoClient("mongodb://"+MONGO_HOST+":"+str(MONGO_PORT), maxPoolSize=None)
+    total = mongo_connection["optimism"]["mev_liquidation_opportunities"].count_documents({})
+    print("Exporting", total, "results...")
+    i = 1
+    for skip in range(0, total, LIMIT):
+        os.system('mongoexport --uri="mongodb://'+MONGO_HOST+':'+str(MONGO_PORT)+'/optimism" --collection mev_liquidation_opportunities --type json --out=mev_liquidation_opportunities_'+str(i)+'.json --skip '+str(skip)+' --limit '+str(LIMIT))
+        i += 1
+    print("Done.")
+
+if __name__ == "__main__":
+    main()
